43494d29c188de794116de9559e3b6eadccbdcbc
lizavr <elizavetarabetskaya@gmail.com>
2023-04-18 00:18:57 +0400

    feat : add vigenere

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -20,13 +20,79 @@ const { NotImplementedError } = require('../extensions/index.js');
  * 
  */
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  constructor(isDirect = true) {
+      this.alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+      this.generateTable();
+      this.isDirect = isDirect;
   }
-  decrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+
+  generateTable() {
+      const arr = [];
+      for (let i = 0; i < this.alphabet.length; i++) {
+          arr.push([]);
+          for (let j = 0; j < this.alphabet.length; j++) {
+              arr[i].push(this.alphabet.charAt(j + i < this.alphabet.length ? j + i : j + i - this.alphabet.length));
+          }
+      }
+      this.table = arr;
+  }
+
+  encrypt(str, code) {
+      if (!str || !code)
+      {
+          throw new Error('Incorrect arguments!');
+      }
+      let result = '';
+      str = str.toUpperCase();
+      code = code.toUpperCase();
+      code = this.createCode(str, code);
+
+      for (let i = 0; i < str.length; i++) {
+          let index = this.alphabet.indexOf(str[i]);
+          if (index === -1) {
+              result += str[i];
+          }
+          else {
+              result += this.table[index][this.alphabet.indexOf(code[i])];
+          }
+      }
+      return this.isDirect ? result : result.split('').reverse().join('');
+
+  }
+  decrypt(str, code) {
+      if (!str || !code)
+      {
+          throw new Error('Incorrect arguments!');
+      }
+      let result = '';
+      str = str.toUpperCase();
+      code = code.toUpperCase();
+      code = this.createCode(str, code);
+
+      for (let i = 0; i < str.length; i++) {
+          let index = this.alphabet.indexOf(code[i]);
+          if (index === -1) {
+              result += str[i];
+          }
+          else {
+              result += this.alphabet[this.table[index].indexOf(str[i])];
+          }
+      }
+      return this.isDirect ? result : result.split('').reverse().join('');
+
+  }
+
+  createCode(str, code) {
+      while (code.length < str.length) {
+          code += code;
+      }
+      str.split('').forEach((item, index) => {
+          if (this.alphabet.indexOf(item) === -1) {
+              code = code.substring(0, index) + item + code.substring(index, code.length - 1);
+          }
+      })
+      code = code.substring(0, str.length);
+      return code;
   }
 }
 
